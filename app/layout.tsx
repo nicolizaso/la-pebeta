ec7cf0048181b063bf19d7ae986355c6468ca278
nicolizaso <nlizaso69@gmail.com>
2026-08-10 20:55:59 -0300

    fix de tipos

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es">
+    <html lang="es" suppressHydrationWarning>
       <body
         className={`${instrumentSerif.variable} ${schibstedGrotesk.variable}`}
       >
